d196c38cb280146b8c60fc99eeccd5890eaef0f2
Matt Hamilton <matt.hamilton49@gmail.com>
2023-02-02 22:33:20 +0100

    removed clgs

--- routes/controllers/review.controller.js
+++ routes/controllers/review.controller.js
@@ -1,30 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const Review = require("../../models/Review.model.js")
+const Review = require("../../models/Review.model.js");
 // const checkIfSamePerson = require("../../utils/checkIfSamePerson");
 
 // needs to be reviews for a specific game, so we might need to populate games?
 exports.listReviews = (req, res, next) => {
-  Review.find().populate("author")
+  Review.find()
+    .populate("author")
     .then((allReviews) => res.json(allReviews))
     .catch((err) => res.json(err));
-
-
-}
-
+};
 
 exports.postCreateForm = (req, res, next) => {
-  const gameId = req.params.id
+  const gameId = req.params.id;
   const newReview = { ...req.body, gameId };
-  console.log(newReview);
   Review.create(newReview)
     .then((newReview) => req.json(newReview))
-    .catch((err) => res.json(err))
-}
-
-
-
+    .catch((err) => res.json(err));
+};
 
 exports.postEditForm = (req, res, next) => {
   const { reviewId } = req.params;
@@ -34,14 +28,11 @@ exports.postEditForm = (req, res, next) => {
     return;
   }
   Review.findByIdAndUpdate(reviewId, req.body, { new: true })
-    .then((updatedReview) =>
-      res.json(updatedReview)
-    )
+    .then((updatedReview) => res.json(updatedReview))
     .catch((err) => res.json(err));
-}
+};
 
 exports.deleteReview = (req, res, next) => {
-  console.log("deleteReview")
   const { reviewId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(reviewId)) {
--- routes/controllers/userController.js
+++ routes/controllers/userController.js
@@ -4,68 +4,43 @@ const mongoose = require("mongoose");
 const User = require("../../models/User.model.js");
 // const checkIfSamePerson = require("../../utils/checkIfSamePerson");
 
-
-
 exports.editUser = (req, res, next) => {
-    console.log("edit user")
-    const { userId } = req.params;
+  const { userId } = req.params;
 
-    // find the user by id and update it
-    User.findByIdAndUpdate(userId, req.body, { new: true })
-        .then((updatedUser) => res.json(updatedUser))
-        .catch((err) => res.json(err));
-}
-
+  // find the user by id and update it
+  User.findByIdAndUpdate(userId, req.body, { new: true })
+    .then((updatedUser) => res.json(updatedUser))
+    .catch((err) => res.json(err));
+};
 
 exports.getUser = (req, res, next) => {
-    console.log("get user")
-    const { userId } = req.params;
+  const { userId } = req.params;
 
-    User.findById(userId)
-        .then((user) => {
-
-
-            res.status(200).json(user)
-        })
-        .catch((err) => res.json(err));
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
+  User.findById(userId)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => res.json(err));
+};
 
 exports.getLoginForm = (req, res, next) => {
-    console.log("get Login Form")
-}
-
+  console.log("get Login Form");
+};
 
 exports.getRegisterForm = (req, res, next) => {
-    console.log("get Registration Form")
-}
-
-
-exports.getProfilePage = (req, res, next) => {
-    console.log("get profile page")
-}
-
+  console.log("get Registration Form");
+};
 
 exports.getProfilePage = (req, res, next) => {
-    console.log("get profile page")
-}
+  console.log("get profile page");
+};
 
+exports.getProfilePage = (req, res, next) => {
+  console.log("get profile page");
+};
 
 exports.getAllUsers = (req, res, next) => {
-    console.log("get all users")
-    User.find()
-        .then((allUsers) => res.json(allUsers))
-        .catch((err) => res.json(err));
-}
+  User.find()
+    .then((allUsers) => res.json(allUsers))
+    .catch((err) => res.json(err));
+};
